63f933e6b0a9e733582c55ac9a9ab0f663c93054
d11416215-rgb <d11416215@gs.takming.edu.tw>
2025-09-24 10:49:30 +0800

    VS 2019

---
+++ D11416215/D11416215/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D11416215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
